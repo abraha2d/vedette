22e8b30c45ea27855c1d9c2881d8a30be388223b
Kevin Abraham <abraha2d@users.noreply.github.com>
2024-04-10 03:27:44 -0400

    Initial commit

---
+++ scripts/build.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env bun
+import esbuild, { type BuildOptions } from "esbuild";
+import { htmlPlugin } from "@craftamap/esbuild-plugin-html";
+import fs from "fs";
+
+// Options common to both entrypoints
+const defaultOptions: BuildOptions = {
+  format: "esm",
+  logLevel: "info",
+  minify: true,
+  outdir: "dist",
+  sourcemap: true,
+  splitting: true,
+};
+
+// Clean build directory
+if (defaultOptions.outdir) {
+  fs.rmSync(defaultOptions.outdir, { recursive: true });
+} else {
+  console.warn("No outdir specified, not cleaning.");
+}
+
+// Build electron entrypoint
+console.log("\nBuilding electron entrypoint...");
+await esbuild.build({
+  ...defaultOptions,
+  entryPoints: ["index.ts"],
+});
+
+// Options specific to main entrypoint
+const options: BuildOptions = {
+  ...defaultOptions,
+  bundle: true,
+  entryNames: "[dir]/[name]-[hash]",
+  entryPoints: ["src/index.tsx"],
+  metafile: true,
+  plugins: [
+    htmlPlugin({
+      files: [
+        {
+          entryPoints: ["src/index.tsx"],
+          filename: "index.html",
+          htmlTemplate: fs.readFileSync("static/index.html").toString(),
+        },
+      ],
+    }),
+  ],
+};
+
+// Build main entrypoint
+console.log("\nBuilding main entrypoint...");
+if (process.argv.includes("--watch")) {
+  const ctx = await esbuild.context(options);
+  await ctx.watch();
+} else {
+  await esbuild.build(options);
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div>
+    <h1>Welcome to Vedette</h1>
+  </div>
+);
+
+export default App;
